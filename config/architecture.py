89b2b654734fee93b996d2fc6a9eae45d503aed0
Russel Dsouza <russel.dsouza47@gmail.com>
2019-08-09 08:39:43 +0530

    Small changes & updates

--- config/architecture.py
+++ config/architecture.py
@@ -1,11 +1,16 @@
 import torch.nn.functional as F
+import torch.nn as nn
 import torch.optim as optim
 
 from . import config as cfg
-from models import SampleNet
+from models import BiLSTM
+from data import liar_plus
 
 
 model_name = 'FakeNewsDetector'
-model = SampleNet().to(cfg.device)
-criterion = F.cross_entropy
+model = BiLSTM(liar_plus.pretrained_vectors,
+               lstm_layer=2,
+               padding_idx='<unk>',
+               hidden_dim=128).to(cfg.device)
+criterion = nn.BCEWithLogitsLoss()
 optimizer = optim.Adam(model.parameters())
--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,22 +1,44 @@
 import pandas as pd
+import os
 from config import config as cfg
 
 
 def prepare_tsv(tsv_path, mode='train'):
     """
-    Make the tsv file more BERT friendly
+    Cleanup TSV files
+
     :param tsv_path: Path to .tsv file
     :param mode: Train/val/test
     :return:
     """
-    df = pd.read_csv(tsv_path, sep='\t')
-    df['statement'] = df.statement.str.replace("\n", " ")
-    df['context'] = df.context.str.replace("\n", " ")
-    df['justification'] = df.justification.str.replace("\n", " ")
+    df = pd.read_csv(tsv_path, sep='\t', names=('id',
+                                                'json_id',
+                                                'label',
+                                                'statement',
+                                                'subjects',
+                                                'speaker',
+                                                'speaker_title',
+                                                'state_info',
+                                                'party',
+                                                'barely_true',
+                                                'false',
+                                                'half_true',
+                                                'mostly_true',
+                                                'pants_on_fire',
+                                                'context',
+                                                'justification'))
+    # Remove incomplete rows
+    df = df.dropna()
 
+    # Remove line endings and replace by space
+    df['statement'] = df['statement'].str.replace("\n", " ")
+    df['context'] = df['context'].str.replace("\n", " ")
+    df['justification'] = df['justification'].str.replace("\n", " ")
+
+    # Save cleaned up files in cache/
     if mode == 'train':
-        df.to_csv('cache/train.tsv', sep='\t', index=False, header=False)
+        df.to_csv(os.path.join(cfg.project_root, 'cache', 'train.tsv'), sep='\t', index=False, header=False)
     elif mode == 'val':
-        df.to_csv('cache/val.tsv', sep='\t', index=False, header=False)
+        df.to_csv(os.path.join(cfg.project_root, 'cache', 'val.tsv'), sep='\t', index=False, header=False)
     elif mode == 'test':
-        df.to_csv('cache/test.tsv', sep='\t', index=False, header=False)
+        df.to_csv(os.path.join(cfg.project_root, 'cache', 'test.tsv'), sep='\t', index=False, header=False)
