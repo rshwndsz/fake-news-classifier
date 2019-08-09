a5ca50f788263a139d25b4430247291922b9aa20
Russel Dsouza <russel.dsouza47@gmail.com>
2019-08-09 13:53:20 +0530

    Add model, criterion, optimizer

--- config/architecture.py
+++ config/architecture.py
@@ -1,4 +1,3 @@
-import torch.nn.functional as F
 import torch.nn as nn
 import torch.optim as optim
 
@@ -8,9 +7,12 @@ from data import liar_plus
 
 
 model_name = 'FakeNewsDetector'
-model = BiLSTM(liar_plus.pretrained_vectors,
+model = BiLSTM(liar_plus.text.vocab.vectors,
                lstm_layer=2,
-               padding_idx='<unk>',
-               hidden_dim=128).to(cfg.device)
+               padding_idx=liar_plus.text.vocab.stoi[liar_plus.text.pad_token],
+               hidden_dim=128
+               ).to(cfg.device)
+
 criterion = nn.BCEWithLogitsLoss()
-optimizer = optim.Adam(model.parameters())
+optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()),
+                       lr=cfg.lr)
