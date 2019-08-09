d0d54b8275e533e859e939acb256bdc37139d1e7
Russel Dsouza <russel.dsouza47@gmail.com>
2019-08-09 20:44:51 +0530

    Add logging to liar_plus

--- data/liar_plus.py
+++ data/liar_plus.py
@@ -1,12 +1,15 @@
 import os
 from torchtext import data, vocab
 import logging
+import coloredlogs
 import spacy
 
 from config import config as cfg
 from . import preprocessing
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('data/liar_plus.py')
+coloredlogs.install(level='DEBUG', logger=logger)
+
 en = spacy.load('en_core_web_sm')
 
 
@@ -21,29 +24,28 @@ preprocessing.prepare_tsv(os.path.join(cfg.dataset_root, 'test2.tsv'), 'test')
 
 
 text = data.Field(lower=True, batch_first=True, tokenize=tokenize)
+label = data.Field(lower=True, sequential=False, use_vocab=False)
+metadata = data.Field()
 fields = [('text', text),
-          ('label', data.Field(lower=True, sequential=False, use_vocab=False, is_target=True)),
-          ('metadata', None)]
+          ('label', label),
+          ('metadata', metadata)]
 
-logger.debug('Reading cleaned TSV files...')
+logger.info('Reading cleaned TSV files...')
 train_set, val_set, test_set = data.TabularDataset.splits(
-                                path=cfg.dataset_root,
-                                train='train2.tsv',
-                                validation='val2.tsv',
-                                test='test2.tsv',
+                                path=cfg.cleaned_dataset_root,
+                                train='train.tsv',
+                                validation='val.tsv',
+                                test='test.tsv',
                                 format='tsv',
                                 fields=fields
                                )
 
-logger.debug('Building vocabulary...')
+logger.info('Building vocabulary...')
 text.build_vocab(train_set, val_set, min_freq=3)
+metadata.build_vocab(train_set, val_set, min_freq=3)
 
-logger.debug('Loading glove vectors...')
-pretrained_vectors = vocab.Vectors(os.path.join(cfg.dataset_root,
-                                                'glove_embeddings/glove.6B.100d.txt'))
-text.vocab.load_vectors(pretrained_vectors)
-
-logger.debug(f'Done preparing datasets.\nVocab Vector shape: {text.vocab.vectors.shape}')
+logger.info('Loading vocab vectors from wiki-news-300d-1M.vec...')
+text.vocab.load_vectors(vocab.Vectors(name='datasets/word_embeddings/wiki-news-300d-1M.vec'))
 
 train_loader = data.BucketIterator(train_set,
                                    batch_size=cfg.batch_size,
@@ -57,6 +59,7 @@ val_loader = data.BucketIterator(val_set,
                                  batch_size=cfg.val_batch_size,
                                  sort_key=lambda x: len(x.text),
                                  device=cfg.device,
+                                 train=False,
                                  shuffle=True,
                                  sort=False
                                  )
@@ -66,4 +69,5 @@ test_loader = data.BucketIterator(test_set,
                                   sort_key=lambda x: len(x.text),
                                   device=cfg.device
                                   )
-logger.debug('Created iterators for train/val/test. Done with data prep!')
+
+logger.info('Created iterators for train/val/test. Done with data prep!')
